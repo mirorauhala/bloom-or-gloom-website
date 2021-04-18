a59eabc955882d34b17e0cdc80e7c93cdeab7562
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-18 17:53:56 +0300

    add boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      'sans': ['Quicksand'],
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
